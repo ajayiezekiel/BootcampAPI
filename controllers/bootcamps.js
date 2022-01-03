36aaeafd369c48080a4d055f8f9d705f52d02ab1
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-03 12:47:17 +0100

    Created controller that allows to get bootcamp within the radius of a specific zipcode

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,12 +1,20 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 const getBootcamps = asyncHandler( async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
+    let query;
+    
+    let queryStr = JSON.stringify(req.query);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+    
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
         
     res.status(200).json({ 
         success: true, 
@@ -80,10 +88,42 @@ const deleteBootcamp = asyncHandler(async (req, res, next) => {
     });
 });
 
+// @desc   Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    console.log(loc);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    // Calc radius using radians
+    // Divide distance by radius of Earth
+    // Earth Radius = 3,963 miles / 6,378 kilometres
+    const radius = distance / 3963;
+
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { 
+            $centerSphere: [[lng, lat], radius] 
+        } }
+    })
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps,
+        error: null
+    })
+});
+
 module.exports = {
     getBootcamps,
     getBootcamp,
     createBootcamp,
     updateBootcamp,
-    deleteBootcamp
+    deleteBootcamp,
+    getBootcampsInRadius
 }
