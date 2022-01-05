65e302d85d3265b777569dbbb8903f61d5e6a402
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-05 16:23:05 +0100

    Added get, post, put and delete operations on course route

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { 
-    getCourses
+    getCourses,
+    getCourse,
+    createCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses');
 
 router
-  .route('/').get(getCourses)
+  .route('/').get(getCourses).post(createCourse);
+
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 
 
