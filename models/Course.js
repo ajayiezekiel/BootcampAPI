130ec818384fcc1ede36188778b4dc4269c6b92e
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-07 14:08:52 +0100

    Added user to the course model

--- models/Course.js
+++ models/Course.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const CourseSchema = new mongoose.Schema({
     title: {
         type: String,
+        unique: true,
         trim: true,
         required: [true, 'Please add a course title']
     },
@@ -34,6 +35,11 @@ const CourseSchema = new mongoose.Schema({
     bootcamp: {
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
         required: true
     }
 });
