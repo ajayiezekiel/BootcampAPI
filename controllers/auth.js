ef54d9e74e114fbc1c919a437f7329576e40f2f0
Ezekiel <ajayiezekiel9@gmail.com>
2022-01-10 19:18:52 +0100

    Added logout controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,6 +51,21 @@ const login = asyncHandler(async (req, res, next) => {
 });
 
 
+// @desc    Log user out / clear cookie
+// @route   GET api/v1/auth/logout
+// @access  Private
+const logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    })
+    
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
+
 // @desc    Get current logged in user
 // @route   POST api/v1/auth/me
 // @access  Private
@@ -207,6 +222,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 module.exports = { 
     register,
     login,
+    logout,
     getMe,
     forgotPassword,
     resetPassword,
